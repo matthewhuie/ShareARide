a27e0e9deb87d1e131c450c0c43c95f91e5b0b6c
matthewhuie <matthew@matthewhuie.com>
2015-02-28 18:25:43 -0500

    Upon opening app, zooms to current location and prints current location

--- app/src/main/java/edu/cmu/andrew/sharearide/MapsActivity.java
+++ app/src/main/java/edu/cmu/andrew/sharearide/MapsActivity.java
@@ -1,21 +1,40 @@
 package edu.cmu.andrew.sharearide;
 
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity {
+import java.io.IOException;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity
+    implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+  private final boolean DEBUG = true;
 
   private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+  private GoogleApiClient mGoogleApiClient;
+  private Location mLastLocation;
+  private double latitude;
+  private double longitude;
 
   @Override
   protected void onCreate (Bundle savedInstanceState) {
     super.onCreate (savedInstanceState);
     setContentView (R.layout.activity_maps);
+    buildGoogleApiClient ();
     setUpMapIfNeeded ();
   }
 
@@ -23,6 +42,18 @@ public class MapsActivity extends FragmentActivity {
   protected void onResume () {
     super.onResume ();
     setUpMapIfNeeded ();
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    mGoogleApiClient.connect();
+  }
+
+  @Override
+  protected void onStop() {
+    mGoogleApiClient.disconnect();
+    super.onStop();
   }
 
   /**
@@ -60,7 +91,50 @@ public class MapsActivity extends FragmentActivity {
    * This should only be called once and when we are sure that {@link #mMap} is not null.
    */
   private void setUpMap () {
-    mMap.addMarker (new MarkerOptions ().position (new LatLng (0, 0)).title ("Marker"));
-    mMap.setMyLocationEnabled(true);
+    mMap.setMyLocationEnabled (true);
+  }
+
+  protected synchronized void buildGoogleApiClient() {
+    mGoogleApiClient = new GoogleApiClient.Builder(this)
+        .addConnectionCallbacks(this)
+        .addOnConnectionFailedListener(this)
+        .addApi(LocationServices.API)
+        .build();
+  }
+
+  @Override
+  public void onConnected (Bundle connectionHint) {
+    mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+        mGoogleApiClient);
+    if (mLastLocation != null) {
+      latitude = mLastLocation.getLatitude ();
+      longitude = mLastLocation.getLongitude();
+
+      // Gets the current location place name
+      Geocoder geoCoder = new Geocoder (this);
+      List <Address> places = null;
+      try {
+        places = geoCoder.getFromLocation (latitude, longitude, 1);
+      } catch (IOException ioe) { }
+      String thisPlace = (places.isEmpty() ? null : places.get (0).getAddressLine (0));
+
+      ((TextView) findViewById (R.id.my_location)).setText ("Current location: " + thisPlace);
+      mMap.moveCamera(CameraUpdateFactory.newLatLngZoom (new LatLng (latitude, longitude), 13));
+    }
+  }
+
+  @Override
+  public void onConnectionSuspended(int cause) {
+    // The connection has been interrupted.
+    // Disable any UI components that depend on Google APIs
+    // until onConnected() is called.
+  }
+
+  @Override
+  public void onConnectionFailed(ConnectionResult result) {
+    // This callback is important for handling errors that
+    // may occur while attempting to connect with Google.
+    //
+    // More about this in the next section.
   }
 }
