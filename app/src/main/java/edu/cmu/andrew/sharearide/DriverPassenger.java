353a7d2a767f376e249a3c8bfed6c8e0e2614aaf
matthewhuie <matthew@matthewhuie.com>
2015-03-25 18:53:25 -0400

    Changed Driver/Passenger page to login

--- app/src/main/java/edu/cmu/andrew/sharearide/DriverPassenger.java
+++ app/src/main/java/edu/cmu/andrew/sharearide/DriverPassenger.java
@@ -1,5 +1,6 @@
 package edu.cmu.andrew.sharearide;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -8,7 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 
 
-public class DriverPassenger extends ActionBarActivity {
+public class DriverPassenger extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,32 +17,9 @@ public class DriverPassenger extends ActionBarActivity {
         setContentView(R.layout.activity_driver_passenger);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_driver_passenger, menu);
-        return true;
-    }
-
-    public void sendMessage(View view){
+    public void login (View view) {
         Intent intent = new Intent(this,MapsActivity.class);
         startActivity(intent);
 
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
