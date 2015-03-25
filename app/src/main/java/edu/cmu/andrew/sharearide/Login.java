b24042dbe6c9b84d097e366cb3d7bc162ec5c3e9
matthewhuie <matthew@matthewhuie.com>
2015-03-25 19:26:17 -0400

    Login Intent updated

--- app/src/main/java/edu/cmu/andrew/sharearide/Login.java
+++ app/src/main/java/edu/cmu/andrew/sharearide/Login.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.RadioButton;
 
 
 public class Login extends ActionBarActivity {
@@ -16,32 +17,11 @@ public class Login extends ActionBarActivity {
         setContentView(R.layout.activity_login);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
-
     public void login(View view){
-        Intent intent = new Intent(this,PassengerHome.class);
-        startActivity(intent);
-
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+      RadioButton passRadio = (RadioButton) findViewById (R.id.passRadio);
+      if (passRadio.isChecked ())
+        startActivity (new Intent(this,PassengerHome.class));
+      else
+        startActivity (new Intent(this,DriverHome.class));
     }
 }
