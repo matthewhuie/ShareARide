2819a8912516167b00c60f0c5becbd7692232b15
Aditi Rao <aditi.rao1288@gmail.com>
2015-03-25 19:25:47 -0400

    Added new activity for driver home

--- app/src/main/java/edu/cmu/andrew/sharearide/DriverHome.java
+++ app/src/main/java/edu/cmu/andrew/sharearide/DriverHome.java
@@ -1,25 +1,130 @@
 package edu.cmu.andrew.sharearide;
 
-import android.support.v7.app.ActionBarActivity;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 
-public class DriverHome extends ActionBarActivity {
+public class DriverHome extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private double latitude;
+    private double longitude;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_home);
+        buildGoogleApiClient ();
+        setUpMapIfNeeded ();
     }
 
+
+    @Override
+    protected void onResume () {
+        super.onResume ();
+        setUpMapIfNeeded ();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_driver_home, menu);
         return true;
+    }
+
+    private void setUpMapIfNeeded () {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager ().findFragmentById (R.id.map))
+                    .getMap ();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap ();
+            }
+        }
+    }
+
+
+    private void setUpMap () {
+        mMap.setMyLocationEnabled (true);
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    public void onConnected (Bundle connectionHint) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            latitude = mLastLocation.getLatitude ();
+            longitude = mLastLocation.getLongitude();
+
+            // Gets the current location place name
+            Geocoder geoCoder = new Geocoder (this);
+            List <Address> places = null;
+            try {
+                places = geoCoder.getFromLocation (latitude, longitude, 1);
+            } catch (IOException ioe) { }
+            String thisPlace = (places.isEmpty() ? null : places.get (0).getAddressLine (0));
+
+            // Prints current location to TextView
+            ((TextView) findViewById (R.id.my_location)).setText ("Current location: " + thisPlace);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom (new LatLng (latitude, longitude), 13));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection has been interrupted.
+        // Disable any UI components that depend on Google APIs
+        // until onConnected() is called.
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // This callback is important for handling errors that
+        // may occur while attempting to connect with Google.
+        //
+        // More about this in the next section.
     }
 
     @Override
