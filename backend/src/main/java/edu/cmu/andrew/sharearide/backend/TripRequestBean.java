24a09f5033386f4dc7ebdb118a04e755b2b2eeda
Aditi Rao <aditi.rao1288@gmail.com>
2015-04-14 20:55:32 -0400

    Trip bean

--- backend/src/main/java/edu/cmu/andrew/sharearide/backend/TripRequestBean.java
+++ backend/src/main/java/edu/cmu/andrew/sharearide/backend/TripRequestBean.java
@@ -3,26 +3,23 @@ package edu.cmu.andrew.sharearide.backend;
 public class TripRequestBean {
 
     private int tripId;
-    private ArrayList<RequestBean> requests = new ArrayList<>();
 
-    public int getTripId() {
+    private int requestId;
+
+    public int getTripId () {
         return tripId;
     }
 
-    public void setTripId(int tripId) {
+    public void setTripId (int tripId) {
         this.tripId = tripId;
     }
 
-    public ArrayList<RequestBean> getRequests() {
-        return requests;
+    public int getRequestId () {
+        return requestId;
     }
 
-  public int getRequestId () {
-    return requestId;
-  }
-
-  public void setRequestId (int requestId) {
-    this.requestId = requestId;
-  }
+    public void setRequestId (int requestId) {
+        this.requestId = requestId;
+    }
 
 }
