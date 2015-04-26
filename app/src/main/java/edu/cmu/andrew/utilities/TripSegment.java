5ba7d7a5940ed39b69dac052f3553fb51485570f
matthewhuie <matthew@matthewhuie.com>
2015-04-26 18:52:36 -0400

    Best route algorithm for new requets

--- app/src/main/java/edu/cmu/andrew/utilities/TripSegment.java
+++ app/src/main/java/edu/cmu/andrew/utilities/TripSegment.java
@@ -1,49 +1,52 @@
 package edu.cmu.andrew.utilities;
 
+import com.google.android.gms.maps.model.LatLng;
+
 /**
  * Created by matthewhuie on 15-04-26.
  */
 public class TripSegment {
 
-  private double sLatitude;
-  private double sLongitude;
-  private double dLatitude;
-  private double dLongitude;
+  private int segmentID;
+  private LatLng source;
+  private LatLng destination;
   private int distance;
   private int duration;
   private int[] passengers;
   private boolean isCompleted;
 
-  public double getsLatitude () {
-    return sLatitude;
+  public TripSegment (int segmentID, LatLng source, LatLng destination, int distance, int duration, int[] passengers, boolean isCompleted) {
+    this.segmentID = segmentID;
+    this.source = source;
+    this.destination = destination;
+    this.distance = distance;
+    this.duration = duration;
+    this.passengers = passengers;
+    this.isCompleted = isCompleted;
   }
 
-  public void setsLatitude (double sLatitude) {
-    this.sLatitude = sLatitude;
+  public int getSegmentID () {
+    return segmentID;
   }
 
-  public double getsLongitude () {
-    return sLongitude;
+  public void setSegmentID (int segmentID) {
+    this.segmentID = segmentID;
   }
 
-  public void setsLongitude (double sLongitude) {
-    this.sLongitude = sLongitude;
+  public LatLng getSource () {
+    return source;
   }
 
-  public double getdLatitude () {
-    return dLatitude;
+  public void setSource (LatLng source) {
+    this.source = source;
   }
 
-  public void setdLatitude (double dLatitude) {
-    this.dLatitude = dLatitude;
+  public LatLng getDestination () {
+    return destination;
   }
 
-  public double getdLongitude () {
-    return dLongitude;
-  }
-
-  public void setdLongitude (double dLongitude) {
-    this.dLongitude = dLongitude;
+  public void setDestination (LatLng destination) {
+    this.destination = destination;
   }
 
   public int getDistance () {
