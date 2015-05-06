39ffff1a0933ff8c18839edf8a39c5f3ab467e2c
Aditi Rao <aditi.rao1288@gmail.com>
2015-05-05 21:05:03 -0400

    comments

--- backend/src/main/java/edu/cmu/andrew/sharearide/backend/UserBean.java
+++ backend/src/main/java/edu/cmu/andrew/sharearide/backend/UserBean.java
@@ -1,5 +1,10 @@
 package edu.cmu.andrew.sharearide.backend;
 
+/**
+ * this class corresponds to all the columns
+ * in the user table
+ */
+
 public class UserBean {
 
   private int userID;
@@ -69,25 +74,57 @@ public class UserBean {
     this.email = email;
   }
 
+    /**
+     * getter for current location longitude of the
+     * user
+     * @return
+     */
+
   public double getLongitude () {
     return longitude;
   }
 
+    /**
+     * setter for current location longitude of the
+     * user
+     * @param longitude
+     */
   public void setLongitude (double longitude) {
     this.longitude = longitude;
   }
+
+    /**
+     * getter for current location latitude of the
+     * user
+     * @return
+     */
 
   public double getLatitude () {
     return latitude;
   }
 
+    /**
+     * setter for current location latitude of the
+     * user
+     * @param latitude
+     */
   public void setLatitude (double latitude) {
     this.latitude = latitude;
   }
 
+    /**
+     * getter for the user type - driver or passenger
+     * @return
+     */
+
   public String getUserType () {
     return userType;
   }
+
+    /**
+     * setter for the user type - driver or passenger
+     * @param userType
+     */
 
   public void setUserType (String userType) {
     this.userType = userType;
