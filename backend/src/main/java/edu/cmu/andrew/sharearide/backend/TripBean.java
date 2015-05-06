19b872516ab158ebb30dc2429f4d9ec90320ab0d
Aditi Rao <aditi.rao1288@gmail.com>
2015-05-05 21:09:20 -0400

    comments

--- backend/src/main/java/edu/cmu/andrew/sharearide/backend/TripBean.java
+++ backend/src/main/java/edu/cmu/andrew/sharearide/backend/TripBean.java
@@ -1,5 +1,10 @@
 package edu.cmu.andrew.sharearide.backend;
 
+/**
+ * this class corresponds to the trip
+ * table in the MySql instance in Google Cloud SQL
+ */
+
 public class TripBean {
 
     private int tripId;
@@ -19,42 +24,89 @@ public class TripBean {
     hasEnded = 0;
   }
 
+    /**
+     * getter for the trip_id column in the trip bean
+     * @return
+     */
+
   public int getTripId() {
         return tripId;
     }
 
+    /**
+     * setter for the trip_id column in the trip bean
+     * @param tripId
+     */
     public void setTripId(int tripId) {
         this.tripId = tripId;
     }
+
+    /**
+     * getter for the driver_user_id column in the trip bean
+     * @return
+     */
 
     public int getDriverUserId() {
         return driverUserId;
     }
 
+    /**
+     * setter for the driver_user_id column in the trip bean
+     * @param driverUserId
+     */
+
     public void setDriverUserId(int driverUserId) {
         this.driverUserId = driverUserId;
     }
+
+    /**
+     * getter for the num_riders column in the trip bean
+     * @return
+     */
 
     public int getNumOfRiders() {
         return numOfRiders;
     }
 
+    /**
+     * setter for the driver_user_id column in the trip bean
+     * @param numOfRiders
+     */
+
     public void setNumOfRiders(int numOfRiders) {
         this.numOfRiders = numOfRiders;
     }
+
+    /**
+     * getter for the is_active column in the trip bean- indicates whether
+     * the trip is still active or not
+     * @return
+     */
 
   public int getIsActive () {
     return isActive;
   }
 
+    /**
+     * setter for the is_active column in the trip bean
+     * @param isActive
+     */
   public void setIsActive (int isActive) {
     this.isActive = isActive;
   }
 
+    /**
+     * getter for the is_ended column
+     * @return
+     */
   public int getHasEnded () {
     return hasEnded;
   }
 
+    /**
+     * setter for the is_ended column
+     * @param hasEnded
+     */
   public void setHasEnded (int hasEnded) {
     this.hasEnded = hasEnded;
   }
